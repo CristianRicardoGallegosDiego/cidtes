e85d6ae3c3055b87b5c7820499d3d1cd95b2b5a2
polar <cristianga2002@ciencias.unam.mx>
2024-06-10 16:00:57 -0600

    Estructura del proyecto terminada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/common/Header/Header.jsx";
+import Footer from "./components/common/Footer/Footer.jsx";
 import "./assets/styles/reset.css";
 import "./assets/styles/App.css";
 
@@ -7,6 +8,8 @@ function App() {
   return (
     <div>
       <Header   />
+      <Footer   />
+
     </div>
   );
 }
