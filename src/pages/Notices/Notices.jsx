1b271d26a6207cd00cd64c21d9d7c67b06796e9e
Polar <cristianga2002@ciencias.unam.mx>
2024-06-27 16:18:29 -0600

    Agregando nueva seccion de avisos

--- src/pages/Notices/Notices.jsx
+++ src/pages/Notices/Notices.jsx
@@ -1,20 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ParallaxScroll } from "../../components/ui/ParallaxScroll";
+import Notice1 from "../../assets/images/pages/notices/noticeone.png";
+import JCEFImg from "../../assets/images/pages/notices/jcef.jpg";
+import "./Notices.css";
 
-const images = [
-    "../../assets/images/pages/notices/a1.jpeg",
-    "../../assets/images/pages/notices/a2.jpeg",
-    "../../assets/images/pages/notices/a3.jpeg",
-    "../../assets/images/pages/notices/a4.jpeg",
-    "../../assets/images/pages/notices/a5.jpeg",
-    "../../assets/images/pages/notices/a6.jpeg"
-  ];
+const NoticeOne = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+const JCEF = () => {
+    return (
+        <div className="jcef-container">
+            <Link to="/jovenes-construyendo-el-futuro" >
+                <h1>Inicio de programa de "Jóvenes Construyendo el Futuro" en CIDTES</h1>
+            </Link>
+            <Link to="/jovenes-construyendo-el-futuro" >
+                <img src={JCEFImg} alt="Jóvenes Construyendo el Futuro" />
+            </Link>
+        </div>
+    );
+};
+
+const CANACO = () => {
+    return (
+        <div className="canaco-container">
+            <div className="coupon-container">
+                <h1>Cuponera CANACO</h1>
+                <iframe 
+                    title="Cuponera CANACO"
+                    src="https://drive.google.com/file/d/1b8veA729CQ3B3hhlcWLwHSvPeuY1MtNO/preview"  
+                    allow="autoplay"
+                >
+                </iframe>
+            </div>
+            <div>
+                <ParallaxScroll />
+            </div>
+        </div>
+    );
+};
 
 const Notices = () => {
     return (
-        <>
-            <ParallaxScroll />
-        </>
+        <section className="notices-main-container">
+            <h1>Avisos CIDTES</h1>
+            <NoticeOne />
+            <JCEF />
+            <CANACO />
+        </section>
     );
 };
 
